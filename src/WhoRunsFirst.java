44f10aa2ef9ac5567492c91bee930500c4f7e95f
GayaniGunasekara <gayanigunasekara2001@gmail.com>
2024-10-21 12:50:58 +0530

    Java Practise Activities

--- src/WhoRunsFirst.java
+++ src/WhoRunsFirst.java
@@ -1,2 +1,14 @@
 public class WhoRunsFirst {
-}
+
+
+        static {
+            System.out.println("Static block is executed");
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Main method is executed");
+        }
+    }
+
+
+
