b5cb6b687e9c5200cc4132fa5c4205d3badc5b62
GayaniGunasekara <gayanigunasekara2001@gmail.com>
2024-10-21 22:02:21 +0530

    Java Practise Activities

---
+++ src/Base.java
@@ -0,0 +1,14 @@
+
+
+    class Base1 {
+        public static void show() {
+            System.out.println("Base class static method");
+        }
+    }
+
+    public class Base {
+        public static void main(String[] args) {
+            Base1.show();
+        }
+
+}
